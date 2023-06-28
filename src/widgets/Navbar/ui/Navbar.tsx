521bb7d395220b8eaf2e0de6d4a743a5df1bce03
victor13bars <victor_bars_bars95@mail.ru>
2023-06-28 17:02:52 +0300

    add Navbar widget

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss'
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to={'/'} className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>
+                    Главная
+                </AppLink>
+                <AppLink to={'/about'} theme={AppLinkTheme.SECONDARY}>
+                    О сайте
+                </AppLink>
+            </div>
+        </div>
+    );
+};
+
