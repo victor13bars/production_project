443d07c1997a950995c718f007035146fc5d168c
victor13bars <victor_bars_bars95@mail.ru>
2023-07-02 16:07:20 +0300

    add hot module replacement plugin.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,4 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "path";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
@@ -16,6 +15,7 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
